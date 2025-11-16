9c8a45a493497d97b8af1e5fb9a666ef2e72decc
Italo Oliveira <italooliveiraxdd@gmail.com>
2025-11-16 13:05:43 -0300

    commit inicial

---
+++ src/main/java/br/com/italooliveira/app_multi_labs/AppMultiLabsApplication.java
@@ -0,0 +1,13 @@
+package br.com.italooliveira.app_multi_labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMultiLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMultiLabsApplication.class, args);
+	}
+
+}
