ea8124a933372a0424989f9ca0b7ffce6444a79e
Italo Oliveira <italooliveiraxdd@gmail.com>
2025-11-18 20:43:28 -0300

    feat: add Dtos de tarefa

---
+++ src/main/java/br/com/italooliveira/app_multi_labs/dtos/TarefaResponseDto.java
@@ -0,0 +1,11 @@
+package br.com.italooliveira.app_multi_labs.dtos;
+
+public record TarefaResponseDto(
+  Long id,
+  String titulo, 
+  String descricao, 
+  String status, 
+  String prioridade
+) {
+  
+}
