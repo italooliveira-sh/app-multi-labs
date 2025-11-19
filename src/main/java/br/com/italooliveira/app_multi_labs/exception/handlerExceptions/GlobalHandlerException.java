c1603151be34f63b9499df187cf6be03e1e1d010
Italo Oliveira <italooliveiraxdd@gmail.com>
2025-11-19 09:41:36 -0300

    fix: ajusta status code de saida nas exceptions dos enums status e prioridade

--- src/main/java/br/com/italooliveira/app_multi_labs/exception/handlerExceptions/GlobalHandlerException.java
+++ src/main/java/br/com/italooliveira/app_multi_labs/exception/handlerExceptions/GlobalHandlerException.java
@@ -8,40 +8,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import br.com.italooliveira.app_multi_labs.exception.PrioridadeTarefaNotFound;
-import br.com.italooliveira.app_multi_labs.exception.StatusTarefaNotFound;
+import br.com.italooliveira.app_multi_labs.exception.PrioridadeTarefaInvalida;
+import br.com.italooliveira.app_multi_labs.exception.StatusTarefaInvalida;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class GlobalHandlerException {
 
-  @ExceptionHandler(PrioridadeTarefaNotFound.class)
-  public ResponseEntity<MensagemPadraoErro> handlerPrioridadeTarefaNotFound(PrioridadeTarefaNotFound ex, 
+  @ExceptionHandler(PrioridadeTarefaInvalida.class)
+  public ResponseEntity<MensagemPadraoErro> handlerPrioridadeTarefaNotFound(PrioridadeTarefaInvalida ex,
                                                                             HttpServletRequest request) {
-    HttpStatus status = HttpStatus.NOT_FOUND;
+    HttpStatus status = HttpStatus.BAD_REQUEST;
 
-    var erro = new MensagemPadraoErro();
-    erro.setPath(request.getRequestURI());
-    erro.setMethod(request.getMethod());
-    erro.setStatus(status.value());
-    erro.setStatusText(status.getReasonPhrase());
-    erro.setMensagem(ex.getMessage());
-    erro.setTimestamp(LocalDateTime.now());
+    var erro = new MensagemPadraoErro(request, status, ex.getMessage());
     return ResponseEntity.status(status).contentType(MediaType.APPLICATION_JSON).body(erro);
   }
   
-  @ExceptionHandler(StatusTarefaNotFound.class)
-  public ResponseEntity<MensagemPadraoErro> handlerStatusTarefaNotFound(StatusTarefaNotFound ex, 
+  @ExceptionHandler(StatusTarefaInvalida.class)
+  public ResponseEntity<MensagemPadraoErro> handlerStatusTarefaNotFound(StatusTarefaInvalida ex,
                                                                         HttpServletRequest request) {
-    HttpStatus status = HttpStatus.NOT_FOUND;
+    HttpStatus status = HttpStatus.BAD_REQUEST;
 
-    var erro = new MensagemPadraoErro();
-    erro.setPath(request.getRequestURI());
-    erro.setMethod(request.getMethod());
-    erro.setStatus(status.value());
-    erro.setStatusText(status.getReasonPhrase());
-    erro.setMensagem(ex.getMessage());
-    erro.setTimestamp(LocalDateTime.now());
+    var erro = new MensagemPadraoErro(request, status, ex.getMessage());
     return ResponseEntity.status(status).contentType(MediaType.APPLICATION_JSON).body(erro);
   }
   
