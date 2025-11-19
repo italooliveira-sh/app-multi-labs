e5e08b4c241ad1e3c12f0d10fdd5c08b9e321533
Italo Oliveira <italooliveiraxdd@gmail.com>
2025-11-19 09:40:05 -0300

    feat: muda o modelo de resposta das exception

--- src/main/java/br/com/italooliveira/app_multi_labs/exception/handlerExceptions/MensagemPadraoErro.java
+++ src/main/java/br/com/italooliveira/app_multi_labs/exception/handlerExceptions/MensagemPadraoErro.java
@@ -1,16 +1,32 @@
 package br.com.italooliveira.app_multi_labs.exception.handlerExceptions;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 @Data
 public class MensagemPadraoErro {
   
   private String path;
-  private String Method;
+  private String method;
   private Integer status;
   private String statusText;
   private String mensagem;
   private LocalDateTime timestamp;
+
+  public MensagemPadraoErro(HttpServletRequest request, HttpStatus status, String mensagem) {
+
+    this.path = request.getRequestURI();
+    this.method = request.getMethod();
+    this.status = status.value();
+    this.statusText = status.getReasonPhrase();
+    this.mensagem = mensagem;
+    this.timestamp = LocalDateTime.now();
+  }
 }
